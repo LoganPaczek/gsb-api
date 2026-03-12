a439fbe01f6b030f789bca2ed57c30c3b9bae9ab
logan <paczek.logan05@gmail.com>
2026-03-12 17:29:58 +0100

    feature/api: create POST method

--- api/visiteur.php
+++ api/visiteur.php
@@ -1,34 +1,37 @@
 <?php
 include_once '../config/global.php';
 include_once '../libs/pdo2.php';
-include_once('../modeles/DAOVisiteurs.php');
+include_once '../entities/Visiteur.php';
+include_once '../modeles/DAOVisiteurs.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-
 switch ($method) {
     case 'GET':
-        //header("Content-Type: application/json");
-
-        //if(!isset($_GET['id'])){
-            //$covoitUsers = getLesCovoitUser();
-            //echo json_encode($covoitUsers, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        //} else {
-            //$covoitUser = getUnCovoitUser($_GET['id']);
-            //echo json_encode($covoitUser, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        //}
-
         break;
     case 'POST':
-        
-       // if(isset($_POST['id']) && isset($_POST['nom']) && isset($_POST['prenom'])){
-         //   $unCovoitUser = new CovoitUser($_POST['id'], $_POST['nom'], $_POST['prenom']);
-           // $ajout = addCovoitUser($unCovoitUser);
-            //return $ajout ? http_response_code(201) : http_response_code(500);
-        //} else {
-            //return http_response_code(400);
-        //}
-
+        // Accepter JSON (Postman, etc.) ou formulaire
+        $data = $_POST;
+        if (empty($data) && strpos($_SERVER['CONTENT_TYPE'] ?? '', 'application/json') !== false) {
+            $raw = file_get_contents('php://input');
+            $data = json_decode($raw, true) ?: [];
+        }
+        if (isset($data['login']) && isset($data['password'])) {
+            $visiteur = new Visiteur(
+                null,
+                $data['login'],
+                $data['password'],
+                null,
+                null,
+                null,
+                0,
+                0
+            );
+            $ajout = addVisiteur($visiteur);
+            http_response_code($ajout ? 201 : 500);
+        } else {
+            http_response_code(400);
+        }
         break;
     case "DELETE" :
         if(isset($_GET['id'])){
