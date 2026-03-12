f268db1efade4ffddd623ff26d856f9c35d40da6
logan <paczek.logan05@gmail.com>
2026-03-12 17:14:39 +0100

    feature/entities: adapt new property on DAO functions

--- tests/modeles/vehicules/testGetVehiculeById.php
+++ tests/modeles/vehicules/testGetVehiculeById.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ . '/../../../modeles/DAOVehicules.php';
+require_once __DIR__ . '/../../../modeles/DAOSaisieJour.php';
+require_once __DIR__ . '/../../../modeles/DAOSaisieHebdo.php';
+require_once __DIR__ . '/../../../modeles/DAOVisiteurs.php';
 require_once __DIR__ . '/../../../entities/Vehicule.php';
+require_once __DIR__ . '/../../../entities/SaisieJour.php';
+require_once __DIR__ . '/../../../entities/SaisieHebdo.php';
+require_once __DIR__ . '/../../../entities/Visiteur.php';
 require_once __DIR__ . '/../../../config/global.php';
 require_once __DIR__ . '/../../../libs/pdo2.php';
 
