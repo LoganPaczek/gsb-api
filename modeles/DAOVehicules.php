e177eab0211ca485b56d6c17d63eeee641c2a2a9
logan <paczek.logan05@gmail.com>
2026-03-12 16:19:51 +0100

    feature/create updateVehicules and deleteVehicules + tests

--- modeles/DAOVehicules.php
+++ modeles/DAOVehicules.php
@@ -28,4 +28,32 @@ function addVehicule($vehicule)
     $stmt->execute();
     return $stmt->rowCount() > 0;
 }
+
+function updateVehicule($vehicule)
+{
+    $pdo = PDO2::getInstance();
+    $stmt = $pdo->prepare("
+        UPDATE vehicules 
+        SET immatriculation = :immatriculation, marque = :marque, modele = :modele
+        WHERE id = :id
+    ");
+    $stmt->bindValue(':immatriculation', $vehicule->getImmatriculation(), PDO::PARAM_STR);
+    $stmt->bindValue(':marque', $vehicule->getMarque(), PDO::PARAM_STR);
+    $stmt->bindValue(':modele', $vehicule->getModele(), PDO::PARAM_STR);
+    $stmt->bindValue(':id', $vehicule->getId(), PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->rowCount() > 0;
+}
+
+function deleteVehicule($id)
+{
+    $pdo = PDO2::getInstance();
+    $stmt = $pdo->prepare("
+        DELETE FROM vehicules 
+        WHERE id = :id
+    ");
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->rowCount() > 0;
+}
 ?>
